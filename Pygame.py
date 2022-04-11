e6b0634cd8e8d5fccf2b1a966bc82434d57372d8
mayiww <102154816+mayiww@users.noreply.github.com>
2022-04-11 11:02:53 +0200

    Update Pygame.py

--- Pygame.py
+++ Pygame.py
@@ -3,41 +3,41 @@ import pygame
 from puissance9 import *
 
 
-P4 = Console()
-pygame.init()
-pygame.font.init()
+def init():
 
 
-screen = pygame.display.set_mode([950, 590])
-playing_screen = pygame.Surface((692, 590))
-info_screen = pygame.Surface((692, 200))
-info_screen.fill((0, 0, 0))
-myfont = pygame.font.SysFont('VCR OSD Mono', 30)
-mymediumfont = pygame.font.SysFont('VCR OSD Mono', 20)
-mylittlefont = pygame.font.SysFont('VCR OSD Mono', 13)
-pygame.display.set_caption("Puissance 4")
+    P4 = Console()
+    pygame.init()
+    pygame.font.init()
 
 
+    screen = pygame.display.set_mode([950, 590])
+    playing_screen = pygame.Surface((692, 590))
+    info_screen = pygame.Surface((692, 200))
+    info_screen.fill((0, 0, 0))
+    myfont = pygame.font.SysFont('VCR OSD Mono', 30)
+    mymediumfont = pygame.font.SysFont('VCR OSD Mono', 20)
+    mylittlefont = pygame.font.SysFont('VCR OSD Mono', 13)
+    pygame.display.set_caption("Puissance 4")
+    jetonRouge = pygame.image.load(sys.path[0] + "/pion_rouge.png")
+    jetonJaune = pygame.image.load(sys.path[0] + "/pion_jaune.png")
+    plateau = pygame.image.load(sys.path[0] + "/plateau.png")
+    playing_screen.blit(plateau, (0, 0))
+    screen.blit(playing_screen, (0, 0))
+    screen.blit(info_screen, (693, 591))
+    screen.blit(myfont.render("Puissance 4", True, (255, 255, 255)), (720, 50))
+    screen.blit(mylittlefont.render("-> Au tour du joueur rouge", True, (255, 255, 255)), (710, 200))
+    screen.blit(mylittlefont.render("   Au tour du joueur jaune", True, (255, 255, 255)), (710, 250))
 
-#Pour ce code, nous utilisons 3 images : le plateau, le pion rouge et le pion jaune, il faut donc au préalable les télécharger
-#Vous les retrouverez sur CheekyNate/Puissance-4.
-
-jetonRouge = pygame.image.load(sys.path[0] + "/pion_rouge.png")
-jetonJaune = pygame.image.load(sys.path[0] + "/pion_jaune.png")
-plateau = pygame.image.load(sys.path[0] + "/plateau.png")
-playing_screen.blit(plateau, (0, 0))
-screen.blit(playing_screen, (0, 0))
-screen.blit(info_screen, (693, 591))
-screen.blit(myfont.render("Puissance 4", True, (255, 255, 255)), (720, 50))
-screen.blit(mylittlefont.render("-> Au tour du joueur rouge", True, (255, 255, 255)), (710, 200))
-screen.blit(mylittlefont.render("   Au tour du joueur jaune", True, (255, 255, 255)), (710, 250))
-
-pygame.display.flip()
+    pygame.display.flip()
+    return screen, playing_screen, info_screen, myfont, mymediumfont, mylittlefont, P4, jetonRouge, jetonJaune
 
 
 
 done = False
+screen, playing_screen, info_screen, myfont, mymediumfont, mylittlefont, P4, jetonRouge, jetonJaune = init()
 while not done:
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -102,7 +102,19 @@ while not done:
                     screen.blit(mymediumfont.render("Le joueur Jaune", True, (255, 255, 0)), (720, 250))
                     screen.blit(mymediumfont.render("  a gagné ! GG", True, (255, 255, 0)), (720, 275))
                     pygame.display.flip()
-                if P4.qui_joue() == 2:
+                    boucle1 = False
+                    
+                    while boucle1 == False:
+                        playing_screen.lock
+                        screen.blit(mymediumfont.render("Appuyez sur Entrée", True, (255, 255, 255)), (715, 400))
+                        screen.blit(mymediumfont.render("   pour rejouer", True, (255, 255, 255)), (715, 450))
+                        pygame.display.flip()
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == K_RETURN:
+                                P4.reset()
+                                screen, playing_screen, info_screen, myfont, mymediumfont, mylittlefont, P4, jetonRouge, jetonJaune = init()
+    
+                else :
                     screen.fill(pygame.Color("black"), (710, 200, 710, 200))
                     screen.fill(pygame.Color("black"), (710, 200, 710, 250))
                     screen.fill(pygame.Color("black"), (710, 400, 710, 400))
@@ -110,6 +122,19 @@ while not done:
                     screen.blit(mymediumfont.render("Le joueur Rouge", True, (255, 0, 0)), (720, 250))
                     screen.blit(mymediumfont.render("  a gagné ! GG", True, (255, 0, 0)), (720, 275))
                     pygame.display.flip()
+                    boucle2 = False
+                    
+                    while boucle2 == False:
+                        playing_screen.lock
+                        screen.blit(mymediumfont.render("Appuyez sur Entrée", True, (255, 255, 255)), (715, 400))
+                        screen.blit(mymediumfont.render("   pour rejouer", True, (255, 255, 255)), (715, 450))
+                        pygame.display.flip()
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == K_RETURN:
+                                P4.reset()
+                                screen, playing_screen, info_screen, myfont, mymediumfont, mylittlefont, P4, jetonRouge, jetonJaune = init()
+
+                        
 
             if not P4.lejeu.jeu_possible():
 
